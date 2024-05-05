71865f1d107ea3661f80abc85a20d7ca187c49df
Keidson Roby <keidsondesigner@gmail.com>
2024-05-04 20:06:59 -0400

    feat: :sparkles: Implementado Form retulizável no Component Edit Produto

--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -1,51 +1,28 @@
 import { Component, inject } from '@angular/core';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/products.interface';
+import { FormComponent } from '../../shared/components/form/form.component';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule
-  ],
+  imports: [FormComponent],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
 export class EditComponent {
-
   productsService = inject(ProductsService);
   matSnackBar = inject(MatSnackBar);
+  // Pego os dados da rota;
   product: Product = inject(ActivatedRoute).snapshot.data['product'];
   router = inject(Router);
 
-  form = new FormGroup({
-    title: new FormControl<string>(this.product.title, {
-      nonNullable: true,
-      validators: Validators.required,
-    }),
-  });
-
-  onSubmit() {
-    console.log('Valor do form', this.form.value);
-    console.log('Valor do input', this.form.controls.title.value);
-
-    this.productsService.put(this.product.id, {
-      title: this.form.controls.title.value,
-    }).subscribe(() => {
+  onSubmit(product:  Omit<Product, 'id'>) {
+    this.productsService.put(this.product.id, product).subscribe(() => {
       this.matSnackBar.open('Produto editado com sucesso!', 'Ok');
-
-      this.form.reset();
       this.router.navigate(['/']);
     })
   }
-
 }
