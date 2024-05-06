d00dd84983f41749136cbedd576a3fb713bc82dc
Keidson Roby <keidsondesigner@gmail.com>
2024-05-05 21:38:05 -0400

    feat: :sparkles: Criado metodo para buscar produto por id ou por title

--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Product } from '../interfaces/products.interface';
+import { Observable, map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  private products: Product[] = [];
 
-  httpClient = inject(HttpClient);
+  constructor(private httpClient: HttpClient) {}
+
 
   getAll() {
     return this.httpClient.get<Product[]>('api/products');
@@ -16,6 +19,16 @@ export class ProductsService {
   getId(id: string) {
     console.log('getId', id);
     return this.httpClient.get<Product>(`api/products/${id}`);
+  }
+
+  // pesquisa produto por id ou por title
+  searchProducts(searchTerm: string): Observable<Product[]> {
+    return this.getAll().pipe(
+      map(products => products.filter(product =>
+        product.id.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        product.title.toLowerCase().includes(searchTerm.toLowerCase())
+      ))
+    );
   }
 
   // TODO: payload"product" é o que é enviado para o backend
