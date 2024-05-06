3e3624584ab10923839fa99519de8e1f1664f7df
Keidson Roby <keidsondesigner@gmail.com>
2024-05-05 21:38:51 -0400

    feat: :sparkles: Implementado busca()  por id ou title

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,31 +1,82 @@
-import { Component, inject } from '@angular/core';
+
+import { Component } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/products.interface';
 import { CardComponent } from './components/card/card.component';
 import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { BehaviorSubject, Observable, Subject, Subscription, debounceTime, shareReplay } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, RouterLink, MatButtonModule],
+  imports: [
+    CardComponent,
+    MatButtonModule,
+    FormsModule,
+    RouterLink,
+    AsyncPipe
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
-  products: Product[] = [];
+  private productsSubject = new BehaviorSubject<Product[]>([]);
+  products$ = this.productsSubject.asObservable();
+  searchSubject = new Subject<string>();
+  filteredProducts$: Observable<Product[]> = new Observable<Product[]>();
+  private searchSubscription: Subscription = new Subscription();
 
-  productsService = inject(ProductsService);
-  router = inject(Router);
-
-  ngOnInit() {
+  constructor(
+    private productsService: ProductsService,
+    private router: Router
+  ) {
     this.loadProducts();
+    this.filteredProducts$ = this.products$.pipe(shareReplay(1));
+    this.searchSubscription = new Subscription();
   }
 
-  loadProducts() {
+  ngOnInit() {
+    this.initializeSearchSubscription();
+  }
+
+  loadProducts(): void {
     this.productsService.getAll().subscribe(products => {
-      this.products = products;
+      // Emite os produtos atualizados para todos os inscritos
+      this.productsSubject.next(products);
     });
+  }
+
+  initializeSearchSubscription() {
+    this.searchSubject.pipe(
+      debounceTime(500)
+    ).subscribe(searchTerm => {
+      if (!searchTerm || searchTerm.length < 3) {
+        this.filteredProducts$ = this.products$; // Show all products
+      } else {
+        this.filteredProducts$ = this.productsService.searchProducts(searchTerm);
+      }
+    });
+  }
+
+  onSearchTermChange(event: Event): void {
+    const searchValue = (event.target as HTMLInputElement).value;
+
+    // Cancela qualquer subscrição existente antes de criar uma nova
+    this.searchSubscription.unsubscribe();
+
+    if (searchValue.length >= 3) {
+      // Emite o valor para o searchSubject
+      this.searchSubject.next(searchValue);
+    } else if (searchValue.length === 0) {
+      // Se o campo de pesquisa for limpo, exibe todos os produtos novamente
+      this.filteredProducts$ = this.products$; // Show all products
+    }
+
+    // Cria uma nova subscrição com debounceTime
+    this.searchSubscription = this.searchSubject.pipe(debounceTime(500)).subscribe();
   }
 
   handleOnEdit(product: Product) {
@@ -34,8 +85,12 @@ export class ListComponent {
 
   handleOnDelete(product: Product) {
     this.productsService.delete(product.id).subscribe(() => {
-      // this.products = this.products.filter(p => p.id !== product.id);
       this.loadProducts();
     });
+  }
+
+  ngOnDestroy(): void {
+    // Garante que a subscrição seja cancelada quando o componente for destruído
+    this.searchSubscription.unsubscribe();
   }
 }
